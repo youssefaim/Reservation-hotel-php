6f6eb992b3eaaa4d1d4c29845d6ba49c2f6cff59
Youssef Aim <147152506+youssefaim@users.noreply.github.com>
2024-06-10 21:30:21 +0100

    Update sup.php

--- sup.php
+++ sup.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new PDO('mysql:host=localhost;dbname=hotel' ,'root','Youssef02');
+    $conn = new PDO('mysql:host=localhost;dbname=hotel' ,'root','');
     $id = $_GET['id_client'];
     $query = $conn->prepare('DELETE FROM CLIENT WHERE id_client=?');
     $exe = $query->execute([$id]);
